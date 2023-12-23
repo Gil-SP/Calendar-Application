e782d1246dc1a6d421caec4307edba966a0a97c0
Gilliana <126738924+Gil-SP@users.noreply.github.com>
2023-12-24 05:44:03 +1100

    completed profile page UI

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -6,13 +6,15 @@ export default function Header() {
   return (
     <div className='bg-stone-300'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/Your-calendar'>
+        <Link to='/your-calendar'>
           <h1 className='font-bold'>Your Calendar</h1>
         </Link>
         <ul className='flex gap-4'>
-          <Link to='/'>
-            <li>Home</li>
-          </Link>
+        {location.pathname !== '/sign-in' && ( // Check if the current route is not '/sign-in'
+            <Link to='/your-calendar'>
+              <li>Home</li>
+            </Link>
+          )}
           <Link to='/profile'>
             {currentUser ? (
               <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
