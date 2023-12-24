e270ff5a40d59ccf7fb6cf537de164a59287c679
Gilliana <126738924+Gil-SP@users.noreply.github.com>
2023-12-24 23:51:56 +1100

    fixed cases

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calendar from './Pages/Calendar';
-import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
