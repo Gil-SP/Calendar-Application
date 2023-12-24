e3406ddb70ef6efd75896aeb997813c7b0423951
Gilliana <126738924+Gil-SP@users.noreply.github.com>
2023-12-25 00:22:16 +1100

    minor changes to sign in page

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -8,12 +7,12 @@ import {
 } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
-export default function Signin() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading, error} = useSelector((state => state.user));
+  const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -42,7 +41,7 @@ export default function Signin() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Welcome Back</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='email'
@@ -60,19 +59,20 @@ export default function Signin() {
         />
         <button
           disabled={loading}
-          className='bg-neutral-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Dont you have an account?</p>
+        <p>Dont have an account?</p>
         <Link to='/sign-up'>
           <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
       <p className='text-red-800 mt-5'>
-        {error ? error.message || 'Something went wrong...' : ""}</p>
+        {error ? error.message || 'Something went wrong...' : ''}
+      </p>
     </div>
   );
 }
